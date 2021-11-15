94ebcf127a59fa3605b2f4d48a6477f4418baf73
Shaxriyor <sherryuser1@gmail.com>
2021-11-15 12:56:34 +0500

    Build contact us page and cookie notice

--- js/main.js
+++ js/main.js
@@ -1,6 +1,82 @@
+/* Mobile Navbar
+ */
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+/*
+ * cookie-notice.js
+ */
+
+void (function (root, factory) {
+  if (typeof define === "function" && define.amd) define(factory);
+  else if (typeof exports === "object") module.exports = factory();
+  else root.CookieNotice = factory();
+})(this, function () {
+  function CookieNotice() {
+    ready(run);
+  }
+
+  CookieNotice.options = {
+    message:
+      "<p>This website uses cookies to help us give you the best experience when you visit. By using this website you consent to our use of these cookies.</p>",
+    dismiss: "Dismiss",
+  };
+
+  function run() {
+    if (window.localStorage.cookieNoticeDismissed) return;
+    show();
+  }
+
+  function dismiss() {
+    var notice = document.getElementById("cookie-notice");
+    if (notice) notice.parentNode.removeChild(notice);
+    window.localStorage.cookieNoticeDismissed = true;
+  }
+
+  function undismiss() {
+    delete window.localStorage.cookieNoticeDismissed;
+  }
+
+  function show() {
+    var $div = document.createElement("div");
+    $div.className = "cookie-notice";
+    $div.id = "cookie-notice";
+
+    var $message = document.createElement("div");
+    $message.className = "cookie-notice-message";
+    $message.innerHTML = CookieNotice.options.message;
+    $div.appendChild($message);
+
+    var $dismiss = document.createElement("button");
+    $dismiss.innerHTML = CookieNotice.options.dismiss;
+    $dismiss.onclick = dismiss;
+    $div.appendChild($dismiss);
+
+    document.body.appendChild($div);
+  }
+
+  function ready(fn) {
+    if (document.readyState === "complete") {
+      return fn();
+    } else if (document.addEventListener) {
+      document.addEventListener("DOMContentLoaded", fn);
+    } else {
+      document.attachEvent("onreadystatechange", function () {
+        if (document.readyState === "interactive") fn();
+      });
+    }
+  }
+
+  CookieNotice.run = run;
+  CookieNotice.dismiss = dismiss;
+  CookieNotice.undismiss = undismiss;
+
+  return CookieNotice;
+});
+
+CookieNotice.undismiss(); // just so it always shows up in this demo
+CookieNotice();
