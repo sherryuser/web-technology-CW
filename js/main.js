8d1fa7f0edd0195f01e2d61aaa2fb6619aaca45e
Shaxriyor <sherryuser1@gmail.com>
2021-12-07 20:09:22 +0500

    Build hosting, my account and contact us page

--- js/main.js
+++ js/main.js
@@ -78,5 +78,18 @@ void (function (root, factory) {
   return CookieNotice;
 });
 
-CookieNotice.undismiss(); // just so it always shows up in this demo
 CookieNotice();
+
+// My Accaunt
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container--account");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
